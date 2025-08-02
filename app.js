52deb50448e3a115827a461af2b6a610fe38646a
Emmanuel <emmanuelalamosn@gmail.com>
2025-08-02 03:17:23 -0600

    Agrega funcion agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombresAgregados = [];
+
+//let nombre = document.querySelector(``);
+document.getElementById(`amigo`).removeAttribute(`disabled`);
+
+
+function agregarAmigo(){
+    let nombre = document.getElementById(`amigo`).value;
+
+    if(nombre.length < 2){
+        console.log(`Introdusca un nombre valido`)
+    }else {
+        console.log("amigo agregado");
+        nombresAgregados.push(nombre);
+    }
+}
+
+function sortearAmigo(){
+
+}
+
+function mostrarNombres(){
+    
+}
